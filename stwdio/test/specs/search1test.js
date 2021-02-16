0eaced07322562771ef54674c4f1f462dc475021
tester-404 <59533456+tester-404@users.noreply.github.com>
2021-02-16 08:06:28 +0800

    Update search1test.js

--- stwdio/test/specs/search1test.js
+++ stwdio/test/specs/search1test.js
@@ -1,6 +1,19 @@
 describe('Ebay Product Search', () => {
 it('should open the main url and verify the title', () => {
 browser.url('https://www.ebay.com/');
-expect(browser).toHaveTitle('Electronics, Cars, Fashion, Collectibles &amp; More | eBay');
+expect(browser).toHaveTitle(
+  'Electronics, Cars, Fashion, Collectibles & More | eBay'
+     );
   })
+
+  it('should search for a product and verify the search text value', () => {
+   const searchInput = $('#gh-ac');
+   const searchBtn = $('#gh-btn');
+
+   searchInput.addValue('Laptop');
+   searchBtn.click();
+
+   expect(searchInput).toHaveValue('Laptop');
+
+      })
 })
